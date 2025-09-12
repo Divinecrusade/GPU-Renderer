ad2555f7edf0a7d8650940adcb4626d52cbc804f
Divinecrusade <gorlovdima@inbox.ru>
2025-09-12 15:21:17 +0300

    refactor: Integrate factory creating crt exceptions in console class

--- Console.cpp
+++ Console.cpp
@@ -34,28 +34,22 @@ Console::Console(std::wstring_view console_window_title) {
   if (errno_t const operation_code_result{freopen_s(&cout_stream, "CONOUT$", "w", stdout)};
       exception::CrtError::OperationFailed(operation_code_result))
       [[unlikely]] {
-#ifdef _DEBUG
-    throw exception::CrtError{__FILEW__, __LINE__, "Cout stream wasn't reopened",
-                              operation_code_result};
-#else
-    throw exception::CrtError{"Console wasn't opened",
-                              operation_code_result};
-#endif  // _DEBUG
+    throw exception::CrtError::Create(operation_code_result,
+                                      "Cout stream wasn't reopened",
+                                      "Console wasn't opened",
+                                      __FILEW__, __LINE__);
   }
   assert(((void)"Cout stream must be initialised", cout_stream != nullptr));
   __assume(cout_stream != nullptr);
-
+  
   FILE* cerr_stream{nullptr};
   if (errno_t const operation_code_result{freopen_s(&cerr_stream, "CONOUT$", "w", stderr)};
       exception::CrtError::OperationFailed(operation_code_result))
       [[unlikely]] {
-#ifdef _DEBUG
-    throw exception::CrtError{__FILEW__, __LINE__,
-                              "Cerr stream wasn't reopened",
-                              operation_code_result};
-#else
-    throw exception::CrtError{"Console wasn't opened", operation_code_result};
-#endif  // _DEBUG
+    throw exception::CrtError::Create(operation_code_result,
+                                      "Cerr stream wasn't reopened",
+                                      "Console wasn't opened",
+                                      __FILEW__, __LINE__);
   }
   assert(((void)"Cout stream must be initialised", cerr_stream != nullptr));
   __assume(cerr_stream != nullptr);
@@ -64,13 +58,10 @@ Console::Console(std::wstring_view console_window_title) {
   if (errno_t const operation_code_result{freopen_s(&cin_stream, "CONIN$", "r", stdin)};
       exception::CrtError::OperationFailed(operation_code_result))
       [[unlikely]] {
-#ifdef _DEBUG
-    throw exception::CrtError{__FILEW__, __LINE__,
-                              "Cin stream wasn't reopened",
-                              operation_code_result};
-#else
-    throw exception::CrtError{"Console wasn't opened", operation_code_result};
-#endif  // _DEBUG
+    throw exception::CrtError::Create(operation_code_result,
+                                      "Cin stream wasn't reopened",
+                                      "Console wasn't opened",
+                                      __FILEW__, __LINE__);
   }
   assert(((void)"Cin stream must be initialised", cin_stream != nullptr));
   __assume(cin_stream != nullptr);
@@ -93,33 +84,27 @@ Console::Console(std::wstring_view console_window_title) {
     return op_status < 0;
   };
   if (SetModeFailed(_setmode(_fileno(stdout), _O_U8TEXT))) [[unlikely]] {
-#ifdef _DEBUG
-    throw exception::CrtError{__FILEW__, __LINE__, "stdout setmode failed",
-                              errno};
-#else
-    throw exception::CrtError{"Console (out and log streams) code page is not set correctly", 
-                              errno};
-#endif  // _DEBUG
+    throw exception::CrtError::Create(errno, 
+                                      "stdout setmode failed",
+                                      "Console code page is not set correctly",
+                                      __FILEW__, __LINE__);
   }
   if (SetModeFailed(_setmode(_fileno(stderr), _O_U8TEXT))) [[unlikely]] {
-#ifdef _DEBUG
-    throw exception::CrtError{__FILEW__, __LINE__,
-                              "stderr setmode failed", 
-                              errno};
-#else
-    throw exception::CrtError{"Console (err stream) code page is not set correctly",
-                              errno};
-#endif  // _DEBUG
+    throw exception::CrtError::Create(errno,
+                                      "stderr setmode failed",
+                                      "Console code page is not set correctly",
+                                      __FILEW__, __LINE__);
   }
 
-  std::ignore = std::ios::sync_with_stdio(true);
+  std::ignore = std::ios::sync_with_stdio();
   std::wcin.clear();
   std::wcout.clear();
   std::wcerr.clear();
   std::wclog.clear();
 
   if (!SetConsoleTitleW(console_window_title.data())) [[unlikely]] {
-    std::wcerr << L"Console title was not set, error code: " << GetLastError()
+    std::wcerr << L"Console title was not set, error code: " 
+               << GetLastError()
                << "\n";
   }
 }
